dfc09b7b0c8eceff644d132655592c13582931cf
btbrad <24836323+btbrad@users.noreply.github.com>
2022-12-20 00:36:12 +0800

    api test

--- mock/login-middleware.js
+++ mock/login-middleware.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log(1111, req.path)
   if (req.method === 'POST' && req.path.indexOf('/login') >= 0) {
     if (req.body.username === 'admin' && req.body.password === '123456') {
       res.status(200).json({
