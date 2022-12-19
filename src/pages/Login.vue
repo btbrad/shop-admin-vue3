295c31dea08917890afd55fc70c1876383eb3763
btbrad <24836323+btbrad@users.noreply.github.com>
2022-12-19 18:34:51 +0800

    样式抽离

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,27 +1,35 @@
 <template>
-  <el-row class="w-screen min-h-screen flex">
-    <el-col :md="16" class="bg-purple-500 flex flex-col justify-center items-center">
+  <el-row class="login-container">
+    <el-col :md="16" class="left-container">
       <div>
-        <p class="text-light-500 font-extrabold text-36px mb-1">WELCOME</p>
+        <p class="slogan">WELCOME</p>
         <p class="text-gray-300">THIS IS FRONTEND PROJECT USING VUE3.X & WINDICSS</p>
       </div>
     </el-col>
-    <el-col :md="8" class="flex flex-col justify-center items-center">
-      <h2 class="text-dark-300 font-bold text-36px mb-4">Thrift Shop</h2>
-      <div class="text-xl text-gray-300 flex items-center justify-center space-x-2 mb-6">
-        <span class="h-1px w-16 bg-gray-200"></span>
+    <el-col :md="8" class="right-container">
+      <h2 class="title">Thrift Shop</h2>
+      <div class="topic">
+        <span class="line"></span>
         <span>账号密码登录</span>
-        <span class="h-1px w-16 bg-gray-200"></span>
+        <span class="line"></span>
       </div>
       <el-form :model="form" class="w-[250px]">
         <el-form-item>
-          <el-input v-model="form.username" placeholder="请输入账号" />
+          <el-input v-model="form.username" placeholder="请输入账号">
+            <template #prefix>
+              <el-icon class="el-input__icon"><User /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
-          <el-input type="password" v-model="form.password" show-password placeholder="请输入密码" />
+          <el-input type="password" v-model="form.password" show-password placeholder="请输入密码">
+            <template #prefix>
+              <el-icon class="el-input__icon"><Lock /></el-icon>
+            </template>
+          </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" round class="w-[250px] text-light-200 hover:text-light-100" color="#7A8FE5" @click="onSubmit">登 录</el-button>
+          <el-button type="primary" round class="login-btn" color="#7A8FE5" @click="onSubmit">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -41,3 +49,30 @@ const onSubmit = () => {
   console.log('submit!')
 }
 </script>
+
+<style scoped>
+.login-container {
+  @apply w-screen min-h-screen flex;
+}
+.login-container .left-container {
+  @apply bg-purple-500 flex flex-col justify-center items-center;
+}
+.login-container .left-container .slogan {
+  @apply text-light-500 font-extrabold text-36px mb-1;
+}
+.login-container .right-container {
+  @apply flex flex-col justify-center items-center;
+}
+.login-container .right-container .title {
+  @apply text-dark-300 font-bold text-36px mb-4;
+}
+.login-container .right-container .topic {
+  @apply text-xl text-gray-300 flex items-center justify-center space-x-2 mb-6;
+}
+.login-container .right-container .topic .line {
+  @apply h-1px w-16 bg-gray-200;
+}
+.login-container .right-container .login-btn {
+  @apply w-[250px] text-light-200 hover:text-light-100;
+}
+</style>
