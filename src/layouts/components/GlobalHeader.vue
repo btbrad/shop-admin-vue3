b95765e67dd53a80c3e5b2a5cdef1104856ee5c6
btbrad <24836323+btbrad@users.noreply.github.com>
2022-12-26 21:02:00 +0800

    全屏和刷新

--- src/layouts/components/GlobalHeader.vue
+++ src/layouts/components/GlobalHeader.vue
@@ -5,12 +5,17 @@
       <span class="ml-2">Thrift Shop</span>
     </div>
     <el-icon class="icon-btn"><Expand /></el-icon>
-    <el-icon class="icon-btn ml-2"><Refresh /></el-icon>
+    <el-tooltip effect="dark" content="刷新" placement="bottom">
+      <el-icon class="icon-btn ml-2" @click="handleRefresh"><Refresh /></el-icon>
+    </el-tooltip>
     <div class="right">
-      <el-icon class="icon-btn mr-2"><FullScreen /></el-icon>
+      <el-tooltip effect="dark" content="全屏" placement="bottom">
+        <el-icon class="icon-btn mr-2" v-if="isFullscreen" @click="toggleFullscreen"><Rank /></el-icon>
+        <el-icon class="icon-btn mr-2" v-else @click="toggleFullscreen"><FullScreen /></el-icon>
+      </el-tooltip>
       <img v-if="store.user.avatar" class="mr-1" :src="store.user.avatar" />
       <el-icon v-else class="text-xl"><Avatar /></el-icon>
-      <el-dropdown class="ml-1">
+      <el-dropdown class="ml-1" @command="handleCommand">
         <span class="el-dropdown-link">
           <span class="text-light-400">{{ store.user.username }}</span>
           <el-icon class="el-icon--right text-light-400">
@@ -19,8 +24,8 @@
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>用户信息</el-dropdown-item>
-            <el-dropdown-item @click="onLogout">退出登录</el-dropdown-item>
+            <el-dropdown-item command="resetPassword">修改密码</el-dropdown-item>
+            <el-dropdown-item command="logout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
@@ -31,6 +36,9 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { mainStore } from '@/store/index.js'
+import { useFullscreen } from '@vueuse/core'
+
+const { isFullscreen, toggle: toggleFullscreen } = useFullscreen()
 
 const store = mainStore()
 const router = useRouter()
@@ -40,6 +48,22 @@ const onLogout = () => {
     router.push('/login')
   })
 }
+
+const handleCommand = (c) => {
+  switch(c) {
+    case 'logout':
+      onLogout()
+      break
+    case "resetPassword":
+      console.log('修改密码')
+      break
+  }
+}
+
+const handleRefresh = () => {
+  location.reload()
+}
+
 </script>
 
 <style scoped>
@@ -54,6 +78,7 @@ const onLogout = () => {
 }
 .icon-btn {
   @apply text-xl;
+  cursor: pointer;
 }
 .right {
   @apply ml-auto mr-4 flex align-middle text-light-500;
